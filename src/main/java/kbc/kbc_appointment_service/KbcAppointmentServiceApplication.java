c1aae5d6222db7a64207a98c52dbb13a339649e0
urfrider <adds04283@gmail.com>
2026-03-06 23:20:54 +0800

    init

---
+++ src/main/java/kbc/kbc_appointment_service/KbcAppointmentServiceApplication.java
@@ -0,0 +1,13 @@
+package kbc.kbc_appointment_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KbcAppointmentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KbcAppointmentServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kbc/kbc_appointment_service/KbcAppointmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package kbc.kbc_appointment_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KbcAppointmentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
